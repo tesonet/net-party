66ada362ee37e84c4dc9c63908c36ecdd25136a3
ziaius <z.racyla.10@aberdeen.ac.uk>
2019-08-09 03:50:04 +0300

    Add service for http requests

---
+++ Net-Party CLI/partycli/HttpService/HttpService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Newtonsoft.Json;
+
+namespace partycli.Http
+{
+    public class HttpService : IHttpService
+    {
+        string m_url = null;
+        public HttpService(string url)
+        {
+            m_url = url;
+        }
+        public async Task<string> GetWithToken(string token)
+        {
+            dynamic httpResponse = await m_url
+                .WithHeader("Accept", "application/json")
+                .WithHeader("Authorization", "Bearer " + token)
+                .GetJsonListAsync();
+
+            return JsonConvert.SerializeObject(httpResponse);
+        }
+
+        public async Task<dynamic> PostJson(string content)
+        {
+            dynamic httpResponse = await m_url
+                .WithHeader("Content-Type", "application/json")
+                .PostAsync(new StringContent(content))
+                .ReceiveJson();
+
+            return httpResponse;
+        }
+    }
+}
---
+++ Net-Party CLI/partycli/HttpService/IHttpService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace partycli.Http
+{
+    public interface IHttpService
+    {
+        Task<string> GetWithToken(string token);
+        Task<dynamic> PostJson(string content);
+    }
+}
