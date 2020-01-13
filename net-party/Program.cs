063725a86598d5a1f3616c7263d350606e01c94d
Simas <simas.maziliauskas@gmail.com>
2020-01-14 00:03:17 +0200

    console command parsing and loggin

--- net-party/Program.cs
+++ net-party/Program.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using net_party.Entities.Database;
 using net_party.Repositories;
 using net_party.Repositories.Contracts;
 using net_party.Services;
@@ -19,6 +19,7 @@ namespace net_party
         {
             var services = new ServiceCollection()
                 .AddLogging()
+
                 // Repositories
                 .AddScoped<IAuthTokenRepository, AuthTokenRepository>()
                 .AddScoped<ICredentialRepository, CredentialRepository>()
@@ -48,34 +49,94 @@ namespace net_party
 
             var logger = serviceProvider.GetService<ILoggerFactory>()
                 .CreateLogger<Program>();
+
             logger.LogDebug("Starting application");
 
-            //var authService = serviceProvider.GetService<IAuthService>();
-            //await authService.AcquireNewToken();
-            var authTokenRepository = serviceProvider.GetService<IAuthTokenRepository>();
-            var token = await authTokenRepository.Get();
-            var serverService = serviceProvider.GetService<IServerService>();
-            var servers = await serverService.GetServers();
-            var localServers = await serverService.GetServers(true);
+            ExecuteApplication(args, serviceProvider);
 
-            foreach (var server in servers)
-            {
-                Console.WriteLine($"{server.Name} {server.Distance}");
-            }
-
-            foreach (var localServer in localServers)
-            {
-                Console.WriteLine($"{localServer.Name} {localServer.Distance}");
-            }
-            //var credentialRepository = serviceProvider.GetService<ICredentialRepository>();
-            //var passwordService = serviceProvider.GetService<IPasswordService>();
-            //var hashed = passwordService.HashPassword("partyanimal");
-            //var user = await credentialRepository.Get("tesonet");
-            //var validated = passwordService.ValidatePassword("partyanimal", user.Password);
-            //var result = await credentialRepository.Add(new Credential() { Username = "tesonet", Password = hashed}) ;
-
-            logger.LogDebug("All done!");
             Console.ReadLine();
+
+            logger.LogDebug("Application done!");
+        }
+
+        private static void ExecuteApplication(string[] args, ServiceProvider services)
+        {
+            var logger = services.GetService<ILoggerFactory>().CreateLogger("Debug");
+
+            var app = new CommandLineApplication(throwOnUnexpectedArg: true);
+            app.Name = "net-party";
+            app.Description = "Application for getting a list of servers.";
+            app.HelpOption("-?|-h|--help");
+
+            app.Command("server_list", (command) =>
+            {
+                Console.WriteLine("server_list");
+                command.Description = "Get a list of servers.";
+                command.HelpOption("-?|-h|--help");
+
+                var local = command.Option("-l|--local",
+                    "Flag the operation to get servers from local storage.",
+                    CommandOptionType.NoValue);
+
+                command.OnExecute(async () =>
+                {
+                    logger.LogInformation("server_list is executing");
+
+                    bool isLocal = local.HasValue();
+
+                    var serverService = services.GetService<IServerService>();
+                    var serverList = await serverService.GetServers(isLocal);
+
+                    foreach(var server in serverList)
+                    {
+                        Console.WriteLine($"Server: {server.Name}");
+                        Console.WriteLine($"Distance: {server.Distance}");
+                    }
+
+                    logger.LogInformation("server_list has finished");
+                    return 0;
+                });
+            });
+
+            app.Command("config", (command) =>
+            {
+                command.Description = "Set the user and password used for authentication.";
+                command.HelpOption("-?|-h|--help");
+
+                var usernameOption = command.Option("--username <value>",
+                    "Username used for authentication.",
+                    CommandOptionType.SingleValue);
+
+                var passwordOption = command.Option("--password <password>", "Password used for authentication", CommandOptionType.SingleValue);
+
+                command.OnExecute(async () =>
+                {
+                    logger.LogInformation("config is executing");
+
+                    string username = usernameOption.Value();
+                    string password = passwordOption.Value();
+
+                    var authService = services.GetService<IAuthService>();
+                    //authService. TODO Set credentials;
+                    Console.WriteLine("executing config");
+                    logger.LogInformation("config has finished");
+                    return 0;
+                });
+            });
+
+            try
+            {
+                logger.LogInformation("Application has begun executing...");
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation("Unable to execute application: {0}", ex.Message);
+            }
         }
     }
 }
